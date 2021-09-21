3c371830a3d225591feae54728911cbc0e7d3a1d
Raffael Sahli <raffael.sahli@doodle.com>
2021-09-21 13:53:55 +0200

    disable leader election by default

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ var (
 func main() {
 	flag.StringVar(&metricsAddr, "metrics-addr", ":9556", "The address of the metric endpoint binds to.")
 	flag.StringVar(&probesAddr, "probe-addr", ":9557", "The address of the probe endpoints bind to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", true,
+	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&leaderElectionNamespace, "leader-election-namespace", "",
